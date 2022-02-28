d9cb665a3ec3d8383526daa52d9024f006e212d9
MatthewLaitinen <mjlaitin@mtu.edu>
2022-02-28 11:39:13 -0500

    Create Profile Splash.php
    added rough copy of account page

---
+++ Profile Splash.php
@@ -0,0 +1,56 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Langlearn - Account Landing</title>
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body class="bg-dark">
+    <nav class="navbar navbar-dark bg-primary navbar-expand sticky-top">
+        <a class="navbar-brand " href="index.html">Langlearn</a> 
+        <div class="collapse navbar-collapse ">
+            <div class="navbar-nav ml-auto">
+                <a class="btn nav-item nav-link" href="html/signin.html">Sign Out</a>
+            </div>
+        </div>
+    </nav>
+    <main class="">
+        <header class="text-center  pb-5 bg-light">
+            <div class="container">
+                <!-- <h1 class="display-1 mb-4 text-primary">Langlearn</h1> -->
+                <p class="display-4 text-dark">Welcome [User]!</p>
+                <a href="html/.html" class="btn btn-primary btn-lg">My Todo List</a> 
+				<a href="html/.html" class="btn btn-primary btn-lg">My Flashcards</a> 
+				<a href="html/.html" class="btn btn-primary btn-lg">My Schedule</a> 
+            </div>
+        </header>
+        <section id="faq" class="bg-dark text-light pt-5">
+            <div id="faq-header" class="container text-center">
+                <h2 class="display-4 pb-3">Frequently Asked Questions</h2>
+                <p></p>
+            </div>
+        </section>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.6/dist/umd/popper.min.js"
+        integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js"
+        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+        crossorigin="anonymous"></script>
+</body>
+<footer class="container">
+
+</footer>
+
+</html>
