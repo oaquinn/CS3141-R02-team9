8fd967dd5c37b0ebf0ffe846735d6a27b178ac06
MatthewLaitinen <mjlaitin@mtu.edu>
2022-03-04 15:39:33 -0500

    Rough Draft Home Page
    Has todo list, friends, notifications, logo

--- php/MainPage.php
+++ php/MainPage.php
@@ -9,28 +9,38 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css"
     integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-<link rel="stylesheet" href="css/app.css">
+<!-- <link rel="stylesheet" href="css/app.css"> -->
 </head>
 <body class="">
+
+	
+
     <nav class="navbar navbar-dark bg-primary navbar-expand sticky-top">
         <a class="navbar-brand " href="../index.html">Langlearn</a>
         <div class="collapse navbar-collapse ">
             <div class="navbar-nav ml-auto">
-                <a class="btn nav-item nav-link" href="html/login.html">Log Out</a>
+                <a class="btn nav-item nav-link" href="html/login.html">Sign Out</a>
             </div>
         </div>
 
     </nav>
 
-	<h1 class="display-1 mb-4 text-primary text-center">Welcome!</h1>
+	<h1 class="display-1 mb-4 text-primary text-center">Langlearn Homepage</h1>
+	
 
-
-	<link rel="stylesheet" href="css/MainPageCSS.css">
+	<!--- https://bbbootstrap.com/snippets/todo-task-list-badges-71324362 -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
 	<div class="row d-flex justify-content-center container">
+	<div class="row">
+	<div class="column">
     <div class="col-md-8">
+	
         <div class="card-hover-shadow-2x mb-3 card">
             <div class="card-header-tab card-header">
-                <div class="card-header-title font-size-lg text-capitalize font-weight-normal"><i class="fa fa-tasks"></i>&nbsp;Task Lists</div>
+                <div class="card-header-title font-size-lg text-capitalize font-weight-normal"><i class="fa fa-tasks"></i>&nbsp;My To-Do List</div>
             </div>
 			
             <div class="scroll-area-sm">
@@ -81,9 +91,80 @@
 			
 			
         </div>
+		
+		
+		
     </div>
+	
+
+
+	</div>
+	
+	
+		
+	
 </div>
 
+<div class="column">
+
+<div class="profilebutton">
+<a href="Profile Splash.php" class="btn btn-primary btn-lg" >My Profile</a> 
+</div>
+
+<ul class="list-group">
+	<p></p><p></p>
+	<p style="text-align:center;"><b>Friends</b></p>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Fred
+    <span class="badge badge-primary badge-pill">14</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Samantha
+    <span class="badge badge-primary badge-pill">2</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Zhang
+    <span class="badge badge-primary badge-pill">1</span>
+  </li>
+</ul>
+
+
+
+
+</div>
+
+<div class="column">
+
+<style>
+#putlogo{
+	position: relative;
+
+}
+#putlogo img{
+	 width: 300px;
+  height: 300px;
+  position: relative;
+  top: 70px;
+  left: 250px;
+}
+</style>
+
+<div id="putlogo">
+<img src="../media/logo.png" /> 
+</div>
+
+</div>
+
+</div>
+
+
+
+<h2> Notifications </h2>
+<ul class="list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Welcome to Langlearn!
+  </li>
+</ul>
 
 </body>
 </html>
