8625a848ed81122ee3e9088dba08d7ee5b77e6f6
oaquinn <98657355+oaquinn@users.noreply.github.com>
2022-03-31 17:19:58 -0400

    Database changes

--- init.sql
+++ init.sql
@@ -1,11 +1,6 @@
 delimiter //
-# This function is designed to be used by the Database admin to make users admins of the website.
-create procedure makeAdmin ( in temp_email varchar(100) ) 
-	begin
-    UPDATE user SET admin = 1 WHERE email = temp_email;
-    end; //
 
-create procedure auth (
+create procedure userAuth (
 	in temp_email varchar(100),
     in temp_password varchar(100)
 	)
@@ -13,11 +8,23 @@ create procedure auth (
     SELECT COUNT(*) FROM user WHERE email = temp_email AND passwd = SHA2(temp_password, 256);
     end; // 
     
-create procedure checkEmail( in temp_email varchar(100) ) 
+create procedure teacherAuth (
+	in temp_email varchar(100),
+    in temp_password varchar(100)
+	)
+    begin
+    SELECT COUNT(*) FROM teacher WHERE email = temp_email AND passwd = SHA2(temp_password, 256);
+    end; // 
+    
+create procedure checkUserEmail( in temp_email varchar(100) ) 
 	begin
     SELECT COUNT(*) FROM user WHERE email = temp_email;
     end; //
-    
+
+create procedure checkTeacherEmail( in temp_email varchar(100) ) 
+	begin
+    SELECT COUNT(*) FROM teacher WHERE email = temp_email;
+    end; //
 create procedure registerUser (
 	in temp_email varchar(100),
     in temp_password varchar(100)
@@ -26,12 +33,64 @@ create procedure registerUser (
     INSERT INTO user VALUES (temp_email, NULL, SHA2(temp_password, 256), 0);
     end; //
 
+create procedure registerTeacher (
+	in temp_email varchar(100),
+    in temp_password varchar(100)
+    )
+    begin 
+    INSERT INTO teacher VALUES (temp_email, NULL, SHA2(temp_password, 256));
+    end; //
+    
 delimiter ;
-# Table of users. PK -  email, password is hash encrypted
+
 create table user(
 	email varchar(100) primary key,
     username varchar(20),
     passwd char(64) not null,
     admin int default 0
-);
+    );
 
+create table teacher(
+	email varchar(100) primary key,
+    username varchar(20),
+    passwd char(64)
+    );
+
+create table course(
+	CRN int auto_increment primary key,
+    email varchar(100),
+    name varchar(30) not null,
+    foreign key (email) references teacher(email)
+    );
+    
+create table assignment(
+	CRN int,
+    name varchar(20),
+    duedate date not null,
+    foreign key (CRN) references course(CRN),
+    primary key(name, CRN)
+    );
+
+create table completed(
+	CRN int primary key,
+    name varchar(20),
+    email varchar(100),
+    foreign key (CRN) references course(CRN),
+    foreign key (email) references user(email)
+    );
+    
+create table takes(
+	CRN int primary key,
+    email varchar(100),
+    foreign key (CRN) references course(CRN),
+    foreign key (email) references user(email)
+    );
+    
+create table teaches(
+	CRN int primary key,
+    email varchar(100),
+    foreign key (CRN) references course(CRN),
+    foreign key (email) references teacher(email)
+    );
+    */
+    
