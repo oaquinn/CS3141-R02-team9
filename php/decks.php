2174c964bf006f6edd87635c0979cb0d3c5be93a
oaquinn <oaquinn@mtu.edu>
2022-04-01 11:15:03 -0400

    added php files

---
+++ php/decks.php
@@ -0,0 +1,75 @@
+<html lang="en">
+
+<!---     Matthew is working on this!!   -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Langlearn - Account Landing</title>
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body class="bg-dark">
+    <nav class="navbar navbar-dark bg-primary navbar-expand sticky-top">
+        <a class="navbar-brand " href="../php/MainPage.php">Langlearn</a> 
+        <div class="collapse navbar-collapse ">
+            <div class="navbar-nav ml-auto">
+                <a class="btn nav-item nav-link" href="html/signin.html">Sign Out</a>
+            </div>
+        </div>
+    </nav>
+    <main class="">
+        <header class="text-center  pb-5 bg-light">
+            <div class="container">
+                <!-- <h1 class="display-1 mb-4 text-primary">Langlearn</h1> -->
+                <p class="display-4 text-dark">Anki Decks</p>
+            </div>
+			 <div class="">
+          
+            <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                   3/16/2022 - https://ankiweb.net/shared/info/276380336
+                </li>
+				<b2> <a class="button button2" id="Uploadfile" href="#">Upload a New Deck</a> </b2>
+            </ul>
+        </div>
+        </header>
+       
+		
+		</div>
+		
+		
+	    </div>
+		</div>
+		
+		
+		
+		
+		
+    </main>
+	
+	
+    
+ 
+</div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.6/dist/umd/popper.min.js"
+        integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js"
+        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+        crossorigin="anonymous"></script>
+</body>
+<footer class="container">
+
+</footer>
+
+</html>
