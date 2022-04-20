c465311c6af53ed47d0f8a91d1606b4604ef2f81
MatthewLaitinen <mjlaitin@mtu.edu>
2022-04-20 11:09:56 -0400

    updated main page name

--- php/MainPage.php
+++ php/MainPage.php
@@ -100,7 +100,7 @@ session_start();
                         <span class="badge badge-primary badge-pill">2</span>
                     </li>
                     <li class="list-group-item d-flex justify-content-between align-items-center">
-                        Zhang
+                        Smith
                         <span class="badge badge-primary badge-pill">1</span>
                     </li>
                 </ul>
