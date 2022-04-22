e7da8e2340b59e31f33abafaf770932067c89f01
oaquinn <98657355+oaquinn@users.noreply.github.com>
2022-04-21 23:34:47 -0400

    changed sign out to back button

--- php/submit.php
+++ php/submit.php
@@ -29,7 +29,7 @@
         <a class="navbar-brand " href="index.php">Langlearn</a>
         <div class="collapse navbar-collapse ">
             <div class="navbar-nav ml-auto">
-                <a class="btn nav-item nav-link" href="login.php">Sign Out</a>
+                <a class="btn nav-item nav-link" href="./studentMainPage.php">Back</a>
             </div>
         </div>
     </nav>
